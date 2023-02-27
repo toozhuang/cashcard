b25afc7256274e38f26c93a1c0504191145d3cda
Zhuang-SF <wangz.today@gmail.com>
2023-02-27 21:42:47 +0800

    feat: Get controller 的一些了解以及对应的test如何写

--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -26,4 +26,13 @@ public class CashCardJsonTest {
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount")
                                         .isEqualTo(123.45);
     }
+
+    @Test
+    public void cashCardDeserializationTest() throws IOException {
+        String expected = "{\"id\":99,\"amount\":123.45}";
+        assertThat(json.parse(expected))
+                .isEqualTo(new CashCard(99L, 123.45));
+        assertThat(json.parseObject(expected).id()).isEqualTo(99);
+        assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
+    }
 }
