cf5eb840e010d2a904b7f976c2288725880821a5
Zhuang-SF <wangz.today@gmail.com>
2023-02-27 20:41:51 +0800

    init: project init

---
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -0,0 +1,13 @@
+package example.cashcard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CashCardJsonTest {
+    @Test
+    public void myFirstTest(){
+        System.out.println("Hello, World!");
+        assertThat(42).isEqualTo(42) ;
+    }
+}
